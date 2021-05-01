4dd16e483f7f4f4f9fa13f6a04f3ebe8821f5023
murad alshorman <muradalshorman@gmail.com>
2021-05-01 22:11:32 +0300

    nocompleted

---
+++ src/views/examples/Landing.js
@@ -0,0 +1,319 @@
+/*!
+
+=========================================================
+* Argon Design System React - v1.1.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-design-system-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-design-system-react/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+// nodejs library that concatenates classes
+import classnames from "classnames";
+import promo1 from "assets/img/theme/promo-1.png";
+import cardImg from "assets/img/theme/img-1-1200x1000.jpg";
+import ill2 from "assets/img/ill/ill-2.svg";
+import team1 from "assets/img/theme/team-1-800x800.jpg";
+import team2 from "assets/img/theme/team-2-800x800.jpg";
+import team3 from "assets/img/theme/team-3-800x800.jpg";
+import team4 from "assets/img/theme/team-4-800x800.jpg";
+
+// reactstrap components
+import {
+  Badge,
+  Button,
+  Card,
+  CardBody,
+  CardImg,
+  FormGroup,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  InputGroup,
+  Container,
+  Row,
+  Col
+} from "reactstrap";
+
+// core components
+import DemoNavbar from "components/Navbars/DemoNavbar.js";
+import CardsFooter from "components/Footers/CardsFooter.js";
+
+// index page sections
+import Download from "../IndexSections/Download.js";
+
+class Landing extends React.Component {
+  state = {};
+  componentDidMount() {
+    document.documentElement.scrollTop = 0;
+    document.scrollingElement.scrollTop = 0;
+    this.refs.main.scrollTop = 0;
+  }
+  render() {
+    return (
+      <>
+        <DemoNavbar />
+        <main ref="main">
+          <div className="position-relative">
+            {/* shape Hero */}
+            <section className="section section-lg section-shaped pb-250">
+              <div className="shape shape-style-1 shape-default">
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+              </div>
+              <Container className="py-lg-md d-flex">
+                <div className="col px-0">
+                  <Row>
+                    <Col lg="6">
+                      <h1 className="display-3 text-white">
+                        My Word {" "}
+                        <span>Small Changes will Make A big differance </span>
+                      </h1>
+                      <p className="lead text-white">
+                        we are learn new things at every moment in our live so that changes we are do it 
+                        now will make us in the future, so find a big challenge to make bigest changes
+
+                      </p>
+                      <div className="btn-wrapper">
+                       
+                      </div>
+                    </Col>
+                  </Row>
+                </div>
+              </Container>
+              {/* SVG separator */}
+              <div className="separator separator-bottom separator-skew">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  preserveAspectRatio="none"
+                  version="1.1"
+                  viewBox="0 0 2560 100"
+                  x="0"
+                  y="0"
+                >
+                  <polygon
+                    className="fill-white"
+                    points="2560 0 2560 100 0 100"
+                  />
+                </svg>
+              </div>
+            </section>
+            {/* 1st Hero Variation */}
+          </div>
+          <section className="section section-lg pt-lg-0 mt--200">
+            <Container>
+              <Row className="justify-content-center">
+                <Col lg="12">
+                  <Row className="row-grid">
+                    <Col lg="4">
+                      <Card className="card-lift--hover shadow border-0">
+                        <CardBody className="py-5">
+                          <div className="icon icon-shape icon-shape-primary rounded-circle mb-4">
+                            <i className="ni ni-check-bold" />
+                          </div>
+                          <h6 className="text-primary text-uppercase">
+                            Download Argon
+                          </h6>
+                          <p className="description mt-3">
+                            Argon is a great free UI package based on Bootstrap
+                            4 that includes the most important components and
+                            features.
+                          </p>
+                          <div>
+                            <Badge color="primary" pill className="mr-1">
+                              design
+                            </Badge>
+                            <Badge color="primary" pill className="mr-1">
+                              system
+                            </Badge>
+                            <Badge color="primary" pill className="mr-1">
+                              creative
+                            </Badge>
+                          </div>
+                          <Button
+                            className="mt-4"
+                            color="primary"
+                            href="#pablo"
+                            onClick={e => e.preventDefault()}
+                          >
+                            Learn more
+                          </Button>
+                        </CardBody>
+                      </Card>
+                    </Col>
+                    
+                   
+                    <Col lg="4">
+                      <Card className="card-lift--hover shadow border-0">
+                        <CardBody className="py-5">
+                          <div className="icon icon-shape icon-shape-warning rounded-circle mb-4">
+                            <i className="ni ni-planet" />
+                          </div>
+                          <h6 className="text-warning text-uppercase">
+                            Prepare Launch
+                          </h6>
+                          <p className="description mt-3">
+                            Argon is a great free UI package based on Bootstrap
+                            4 that includes the most important components and
+                            features.
+                          </p>
+                          <div>
+                            <Badge color="warning" pill className="mr-1">
+                              marketing
+                            </Badge>
+                            <Badge color="warning" pill className="mr-1">
+                              product
+                            </Badge>
+                            <Badge color="warning" pill className="mr-1">
+                              launch
+                            </Badge>
+                          </div>
+                          <Button
+                            className="mt-4"
+                            color="warning"
+                            href="#pablo"
+                            onClick={e => e.preventDefault()}
+                          >
+                            Learn more
+                          </Button>
+                        </CardBody>
+                      </Card>
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+            </Container>
+          </section>
+          
+          
+          <section className="section pb-0 bg-gradient-warning">
+            <Container>
+              <Row className="row-grid align-items-center">
+                <Col className="order-lg-2 ml-lg-auto" md="6">
+                  <div className="position-relative pl-md-5">
+                    <img
+                      alt="..."
+                      className="img-center img-fluid"
+                      src={ill2}
+                    />
+                  </div>
+                </Col>
+                <Col className="order-lg-1" lg="6">
+                  <div className="d-flex px-3">
+                    <div>
+                      <div className="icon icon-lg icon-shape bg-gradient-white shadow rounded-circle text-primary">
+                        <i className="ni ni-building text-primary" />
+                      </div>
+                    </div>
+                    <div className="pl-4">
+                      <h4 className="display-3 text-white">Modern Interface</h4>
+                      <p className="text-white">
+                        The Arctic Ocean freezes every winter and much of the
+                        sea-ice then thaws every summer, and that process will
+                        continue whatever.
+                      </p>
+                    </div>
+                  </div>
+                  <Card className="shadow shadow-lg--hover mt-5">
+                    <CardBody>
+                      <div className="d-flex px-3">
+                        <div>
+                          <div className="icon icon-shape bg-gradient-success rounded-circle text-white">
+                            <i className="ni ni-satisfied" />
+                          </div>
+                        </div>
+                        <div className="pl-4">
+                          <h5 className="title text-success">
+                            Awesome Support
+                          </h5>
+                          <p>
+                            The Arctic Ocean freezes every winter and much of
+                            the sea-ice then thaws every summer, and that
+                            process will continue whatever.
+                          </p>
+                          <a
+                            className="text-success"
+                            href="#pablo"
+                            onClick={e => e.preventDefault()}
+                          >
+                            Learn more
+                          </a>
+                        </div>
+                      </div>
+                    </CardBody>
+                  </Card>
+                  <Card className="shadow shadow-lg--hover mt-5">
+                    <CardBody>
+                      <div className="d-flex px-3">
+                        <div>
+                          <div className="icon icon-shape bg-gradient-warning rounded-circle text-white">
+                            <i className="ni ni-active-40" />
+                          </div>
+                        </div>
+                        <div className="pl-4">
+                          <h5 className="title text-warning">
+                            Modular Components
+                          </h5>
+                          <p>
+                            The Arctic Ocean freezes every winter and much of
+                            the sea-ice then thaws every summer, and that
+                            process will continue whatever.
+                          </p>
+                          <a
+                            className="text-warning"
+                            href="#pablo"
+                            onClick={e => e.preventDefault()}
+                          >
+                            Learn more
+                          </a>
+                        </div>
+                      </div>
+                    </CardBody>
+                  </Card>
+                </Col>
+              </Row>
+            </Container>
+            {/* SVG separator */}
+            <div className="separator separator-bottom separator-skew zindex-100">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                preserveAspectRatio="none"
+                version="1.1"
+                viewBox="0 0 2560 100"
+                x="0"
+                y="0"
+              >
+                <polygon
+                  className="fill-white"
+                  points="2560 0 2560 100 0 100"
+                />
+              </svg>
+            </div>
+          </section>
+          
+         
+          
+      
+        </main>
+        <CardsFooter />
+      </>
+    );
+  }
+}
+
+export default Landing;
